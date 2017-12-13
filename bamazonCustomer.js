488e27830b5a1f95c00ff2e3512bd15a4da813dd
Melodie Chi <Melodie@Apple.local>
2017-12-13 13:02:38 -0800

    ask for item ID and confirm the product chosen

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,18 +12,45 @@ var connection = mysql.createConnection({
 connection.connect((err) => {
     if (err) throw err;
     console.log('Connection successful');
-    start();
+    displayItems();
 });
 
 var displayItems = function() {
-    connection.query('SELECT * FROM products', (err, res) => {
+    connection.query(`SELECT item_id, product_name, price FROM products`, (err, res) => {
         // console.log(res);
         for (var i = 0; i < res.length; i++) {
             console.log(chalk.blue.bold(`\n\tItem ID: ${res[i].item_id}\n\tProduct Name: ${res[i].product_name}\n\tPrice: $${res[i].price}\n\n--------------------------------`));
         }
+        start();
     });
 };
 
 var start = function() {
-    displayItems();
+    inquirer.prompt({
+        name: 'itemID',
+        type: 'input',
+        message: 'Enter the ID of the item you would like to purchase: ',
+        validate: (value) => {
+            if (!isNaN(value)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }).then((answer) => {
+        connection.query('SELECT item_id, product_name, price FROM products WHERE ?', { item_id: answer.itemID }, (err, res) => {
+            inquirer.prompt({
+                name: 'confirmItem',
+                type: 'confirm',
+                message: `You chose` + chalk.cyan(` '${res[0].product_name}'. `) + `Is this correct?`
+            }).then((answer) => {
+                if (answer.confirmItem) {
+                    console.log('hello');
+                }
+                else {
+                    console.log('oops');
+                }
+            });
+        });
+    });
 };

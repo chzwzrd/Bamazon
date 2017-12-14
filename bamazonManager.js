2387a7388d41e1352427265da4770c3bae19ce09
Melodie Chi <Melodie@Apple.local>
2017-12-13 20:04:18 -0800

    add to inventory working

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,11 +15,21 @@ connection.connect((err) => {
     if (err) throw err;
     console.log('Connection successful');
     // display all items from database once mysql connection has been established
+    resetCart();
     displayMenu();
 });
 
+// GLOBAL VARIABLES
+// =====================================================================================
+var itemList = [];
+var chosenItem = [];
+
 // FUNCTIONS
 // =====================================================================================
+var resetCart = function() {
+    chosenItem = [];
+}
+
 var displayMenu = function() {
     inquirer.prompt({
         name: 'action',
@@ -72,9 +82,75 @@ var viewLowInventory = function() {
 };
 
 var addToInventory = function() {
-
+    askForID();
 };
 
 var addNewProduct = function() {
-
+    
 };
+
+var askForID = function() {
+    inquirer.prompt({
+        name: 'itemID',
+        type: 'input',
+        message: 'Enter the ID of the item you\'d like to restock:',
+        // validate input is number from 1-10
+        validate: (value) => {
+            if (!isNaN(value) && (value > 0 && value <= 10)) {
+                return true;
+            } else {
+                console.log(chalk.red(' => Please enter a number from 1-10'));
+                return false;
+            }
+        }
+        // select all rows where ID = user's input
+    }).then((answer) => {
+        connection.query('SELECT * FROM products WHERE ?', { item_id: answer.itemID }, (err, res) => {
+            confirmItem(res[0].product_name, res);
+        });
+    });
+};
+
+var confirmItem = function(product, object) {
+    inquirer.prompt({
+        name: 'confirmItem',
+        type: 'confirm',
+        message: `You chose` + chalk.blue.bold(` '${product}'. `) + `Is this correct?`
+    }).then((answer) => {
+        if (answer.confirmItem) {
+            chosenItem.push(object);
+            askHowMany();
+        } else {
+            askForID();
+        }
+    });
+};
+
+var askHowMany = function() {
+    inquirer.prompt({
+        name: 'howMany',
+        type: 'input',
+        message: 'Enter the quantity you would like to add:',
+        validate: (value) => {
+            if (!isNaN(value) && value > 0) {
+                return true;
+            } else {
+                console.log(chalk.red(' => Oops, please enter a number greater than 0'));
+                return false;
+            }
+        }
+    }).then((answer) => {
+        chosenItem.push(answer.howMany);
+        connection.query('UPDATE products SET ? WHERE ?', [
+            {
+                stock_quantity: Number(chosenItem[0][0].stock_quantity) + Number(answer.howMany)
+            },
+            {
+                item_id: chosenItem[0][0].item_id
+            }
+        ], (err, res) => {
+            console.log(chalk.blue.bold(`Inventory updated! Item ${chosenItem[0][0].item_id} now has ${Number(chosenItem[0][0].stock_quantity) + Number(chosenItem[1])} items in stock`));
+            connection.end();
+        });
+    });
+}
